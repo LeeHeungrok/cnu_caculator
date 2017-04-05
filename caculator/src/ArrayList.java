46848b04f113edd0906780500071a3ed408dfb77
Dongho Nam <sirius0916@gmail.com>
2017-04-05 20:39:26 +0900

    add method_isEmpty and clear

--- caculator/src/ArrayList.java
+++ caculator/src/ArrayList.java
@@ -65,14 +65,12 @@ public class ArrayList<T> implements Stack {
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return (this._top == -1);
 	}
 
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
-		
+		this._top = -1;
+		this._elements = null;
 	}
-	
 }
